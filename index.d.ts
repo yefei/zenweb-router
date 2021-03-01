0279f018677f33681888563772c3d02458a3d4cc
YeFei <316606233@qq.com>
2021-03-01 16:41:52 +0800

    d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,11 @@
-import '@zenweb/core';
+import { Core } from '@zenweb/core';
 import * as KoaRouter from 'koa-router';
+
+export interface RouterOptions {
+  discoverPaths?: string[];
+}
+
+export declare function setup(core: Core, options?: RouterOptions): void;
 
 declare module '@zenweb/core' {
   interface Core {
